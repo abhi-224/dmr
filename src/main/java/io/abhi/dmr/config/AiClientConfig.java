c36b0dcd7dc71f2ecfded8a7b3efccbfb904c04b
Abhi <meabhi224@gmail.com>
2026-02-25 19:45:04 +0545

    added memory to client config

--- src/main/java/io/abhi/dmr/config/AiClientConfig.java
+++ src/main/java/io/abhi/dmr/config/AiClientConfig.java
@@ -2,7 +2,10 @@ package io.abhi.dmr.config;
 
 import io.abhi.dmr.advisors.TokenUsageAdvisor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.client.advisor.api.BaseChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -13,9 +16,13 @@ import org.springframework.context.annotation.Configuration;
 public class AiClientConfig {
 
   @Bean(name = "qwenChatClient")
-  public ChatClient qwenChatClient(@Qualifier("qwenModel") OllamaChatModel qwenModel) {
+  public ChatClient qwenChatClient(
+      @Qualifier("qwenModel") OllamaChatModel qwenModel, ChatMemory chatMemory) {
+
+    BaseChatMemoryAdvisor chatMemoryAdvisor = MessageChatMemoryAdvisor.builder(chatMemory).build();
+
     return ChatClient.builder(qwenModel)
-        .defaultAdvisors(new TokenUsageAdvisor(), new SimpleLoggerAdvisor())
+        .defaultAdvisors(chatMemoryAdvisor, new TokenUsageAdvisor(), new SimpleLoggerAdvisor())
         .defaultOptions(ChatOptions.builder().maxTokens(2000).temperature(0.2).build())
         .build();
   }
